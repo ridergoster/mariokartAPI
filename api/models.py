5fa43e6d5d7ba8a89be1d43d3ba74754e9ac6d48
Baptiste <baptiste.deleris78@gmail.com>
2017-06-09 18:46:44 +0200

    fix game model

--- api/models.py
+++ api/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Game(models.Model):
     name = models.CharField(max_length=50)
     abbreviation = models.CharField(max_length=10)
-    released_date = models.DateField
+    released_date = models.DateField()
     platform = models.CharField(max_length=15)
-    cover = models.URLField
+    cover = models.URLField(blank=True)
 
     def __str__(self):
         return "%s" % self.name
