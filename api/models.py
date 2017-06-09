4a6ef90ec2af55bc7b10ec729d2b217ed805a8a0
Vincent Kocupyr <vincent.kocupyr@gmail.com>
2017-06-09 12:27:50 +0200

    add character model

--- api/models.py
+++ api/models.py
@@ -15,7 +15,11 @@ class Game(models.Model):
 class Cup(models.Model):
     name = models.CharField(max_length=50)
     img_url = models.CharField(max_length=500)
-    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='cups')
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE,
+        related_name='cups',
+    )
     retro = models.BooleanField(default=False)
 
 
@@ -24,3 +28,48 @@ class Circuit(models.Model):
     cup = models.ManyToManyField(Cup)
     nb_laps = models.IntegerField(default=3)
     img_url = models.CharField(max_length=500)
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=30)
+    birthDate = models.DateTimeField()
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('U', 'Unknow'),
+    )
+    WEIGHT = (
+        ('L', 'Light'),
+        ('M', 'Medium'),
+        ('H', 'Heavy'),
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER,
+        default='U',
+    )
+    weight = models.CharField(
+        max_length=1,
+        choices=WEIGHT,
+        default='M',
+    )
+
+    def __str__(self):
+        return "%s" % (self.name)
+
+
+class Statistic(models.Model):
+    character = models.OneToOneField(
+        Character,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    hours = models.IntegerField(
+        default=0,
+    )
+    avg_position = models.IntegerField(
+        default=8,
+    )
+    nb_use = models.IntegerField(
+        default=0,
+    )
