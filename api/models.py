81aeb0376a9a3d65c9957009f88294e5f2504b93
Grunt <lacrym095@gmail.com>
2017-06-09 12:11:09 +0200

    Added models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Cup(models.Model):
+    name = models.CharField(max_length=50)
+    img_url = models.CharField(max_length=500)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='cups')
+    retro = models.BooleanField(default=False)
+
+class Circuit(models.Model):
+    name = models.CharField(max_length=50)
+    cup = models.ManyToManyField(Cup)
+    nb_laps = models.IntegerField(default=3)
+    img_url = models.CharField(max_length=500)
