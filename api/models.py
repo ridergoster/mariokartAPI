a978941955009fa3779c1c20f437c1f96b0ac1ba
Baptiste Deléris <baptiste.deleris78@gmail.com>
2017-06-09 12:16:48 +0200

    add game model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=10)
+    released_date = models.DateField
+    platform = models.CharField(max_length=15)
+    cover = models.URLField
+
+    def __str__(self):
+        return "%s" % self.name
